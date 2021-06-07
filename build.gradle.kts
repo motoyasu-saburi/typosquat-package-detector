6152a072afd7a1bc567222f705f16cb4ac3e685b
motoyasu-saburi <kumagoro_alice@yahoo.co.jp>
2021-06-07 21:45:18 +0900

    add http client and test libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,14 +10,25 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
-
-
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
 
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
 application {
     mainClassName = "MainKt"
+}
+
+dependencies {
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.4.3")
+    testImplementation("io.kotest:kotest-assertions-core:.4.4.3")
+    testImplementation("com.squareup.okhttp3:mockwebserver:5.0.0-alpha.2")
+
+    implementation("com.github.kittinunf.fuel:fuel:2.3.1")
 }
